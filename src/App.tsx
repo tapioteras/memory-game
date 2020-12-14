a308aa580457e56015b87314905b8b8fe80874b1
Tapio Teras <tapio.teras@reaktor.com>
2020-12-14 19:49:18 +0200

    Add on click handler for each card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Box, ChakraProvider, SimpleGrid} from "@chakra-ui/react";
 
-const withCardContents = () => <Box padding={2}>image here</Box>
+const withCardContents = i => <Box padding={2} onClick={() => alert(i)}>image here</Box>
 
 const generateDeck = (amount: number = 24) =>
   <SimpleGrid columns={6} spacing={4}>
