084706fb1ccecfc94aef3b9c9b7e6144ac1a7166
Tapio Teras <tapio.teras@reaktor.com>
2020-12-14 19:35:23 +0200

    Create card deck generator function seed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import {ChakraProvider} from "@chakra-ui/react";
+import {ChakraProvider, Box} from "@chakra-ui/react";
+
+const generateDeck = (amount: number = 24) => Array.from(Array(amount), (_,x) => x).map((k, i) => <Box>card {i + 1}</Box>)
 
 const App = () =>
   <ChakraProvider>
-    test
+    {generateDeck()}
   </ChakraProvider>
 
 export default App;
