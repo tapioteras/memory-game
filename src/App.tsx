7a2cf362cc370c2205560bcbc2197f0926498fab
Tapio Teras <tapio.teras@reaktor.com>
2020-12-14 20:05:57 +0200

    Generate card pairs

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
 import * as React from 'react';
 import {Box, ChakraProvider, SimpleGrid} from "@chakra-ui/react";
+import {useState} from "react";
 
-const withCardContents = i => <Box padding={2} onClick={() => alert(i)}>image here</Box>
+const generateCards = (amount: number) => {
+  const cards = Array
+    .from(Array(amount), (_, x) => x)
+    .map((k, i) => ({
+      id: i + 1,
+      image: `image-number-${i + 1}`
+    }))
+  return [
+    ...cards.map((c) => ({...c, pair: 1})),
+    ...cards.map((c) => ({...c, pair: 2}))
+  ]
+}
 
-const generateDeck = (amount: number = 24) =>
-  <SimpleGrid columns={6} spacing={4}>
-    {Array
-      .from(Array(amount), (_, x) => x)
-      .map(withCardContents)
+const generateDeck = (amount: number = 24) => {
+  const [cards] = useState([...generateCards(amount)])
+  console.log(cards)
+  return <SimpleGrid columns={6} spacing={4}>
+    {cards
       .map((contents, i) =>
         <Box
           key={`card-${i}`}
           bg="tomato"
           height={100}
           width={100}>
-          {contents}
+          <Box padding={2} onClick={() => alert(i)}>
+            {contents.image}
+          </Box>
         </Box>)}
   </SimpleGrid>
+}
 
 const App = () =>
   <ChakraProvider>
