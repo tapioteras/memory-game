06907e419612155379223255e7847c31b6d137aa
Tapio Teras <tapio.teras@reaktor.com>
2020-12-15 10:55:04 +0200

    Fix card flipping logic

--- src/App.tsx
+++ src/App.tsx
@@ -25,21 +25,23 @@ const generateDeck = (amount: number = 24) => {
     {cards
       .map((contents, i) =>
         <Box
+          onClick={() => {
+            if (!flipped1) {
+              setFlipped1(contents)
+            } else if (!flipped2) {
+              setFlipped2(contents)
+            } else {
+              console.log("over")
+              setFlipped1(null)
+              setFlipped2(null)
+            }
+          }}
           key={`card-${i}`}
           bg={[toIdPair(flipped1), toIdPair(flipped2)].includes(toIdPair(contents)) ? "white" : "tomato"}
           height={100}
           width={100}>
+          <Box padding={2}>
             {contents.image}
-          <Box padding={2} onClick={() => {
-            if (flipped1) {
-              setFlipped2(contents)
-            } else if (flipped2) {
-              setFlipped1(null)
-              setFlipped2(null)
-            } else {
-              setFlipped1(contents)
-            }
-          }}>
           </Box>
         </Box>)}
   </SimpleGrid>
